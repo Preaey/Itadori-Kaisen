ff0a70225dd177e645a886750b806805e49df752
Preaey <161577069+Preaey@users.noreply.github.com>
2024-02-29 13:03:44 +0200

    Add files via upload

---
+++ COSC 101 ASSIGNMENT.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Name: Shedrack Clinton Prince\n");
+    printf("Matric No: 159535\n");
+    printf("Departmental: Information Technology\n");
+    return 0;
+}
